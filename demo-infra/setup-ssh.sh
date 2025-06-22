cb36e6712c28881dd12e08e4332358653b5adb19
ndaeila <ndaeila@uw.edu>
2025-06-21 18:30:20 -0700

    Created the ovs switch for better or worse. Took 6 hours

--- demo-infra/setup-ssh.sh
+++ demo-infra/setup-ssh.sh
@@ -2,6 +2,6 @@
 
 rm -rf test-ssh-keys/*
 mkdir -p test-ssh-keys
-ssh-keygen -t rsa -b 4096 -f test-ssh-keys/id_rsa -N "" -C "frr-router" -q
+ssh-keygen -t rsa -b 4096 -f test-ssh-keys/id_rsa -N "" -C "network-devices" -q
 cp test-ssh-keys/id_rsa.pub test-ssh-keys/authorized_keys
 echo "SSH keys generated" 
